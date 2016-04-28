c79e5746dc5218353912a02302e6392d837406b4
Michael Cho <micoolcho21@gmail.com>
2016-04-27 23:25:38 -0700

    Route root to home page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'pages/home', to: 'pages#home'
+  root 'pages#home'
   get 'pages/about', to: 'pages#about'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
