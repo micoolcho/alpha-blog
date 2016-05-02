8f9ccaba66908a16a3efe22e88cb85551ddb3dc6
Michael Cho <micoolcho21@gmail.com>
2016-05-02 16:58:12 -0700

    Signup users UI

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/about', to: 'pages#about'
 
+  get 'signup', to: 'users#new'
+  #post 'users', to: 'users#create'
+  resources :users, except: [:new]
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
